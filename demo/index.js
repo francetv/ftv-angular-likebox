1417c03e3af73676a8d621ec338880ef57d61090
Thierry Thuon <thierry.thuon.ext@francetv.fr>
2016-04-20 15:03:25 +0200

    add alert

--- demo/index.js
+++ demo/index.js
@@ -7,6 +7,7 @@ angular.module('demoApp').controller('DemoController', ['$scope', '$rootScope', 
 
     $rootScope.$on('ftv-likebox-update', function(event, data) {
         console.log(data);
+        alert('ftv-likebox-update event is triggered');
         $scope.currentState = data;
     })
 }]);
